12ea9405d0d59c3913ad20a6f76f291a97e78761
TonMoshi <chingowamen@hotmail.com>
2017-01-24 10:10:15 +0100

    1.3

---
+++ src/View/Units/Defense.java
@@ -0,0 +1,16 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package View.Units;
+
+import no.geosoft.cc.graphics.GObject;
+
+/**
+ *
+ * @author dam2
+ */
+public class Defense extends GObject{
+    
+}
